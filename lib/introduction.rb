16dc330a6c9ec2e5400ab2c9c73fa972ddef9541
amusing-bolt-9198 <edmund.nelson.b@gmail.com>
2019-01-07 02:06:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 def introduction (name)
-  output = "Hi my name is #{name}."
-  puts output
+  puts "Hi my name is #{name}."
+  
 end
 def introduction_with_language(name, language)
   final = "Hi, my name is #{name} and I am learning to program in #{language}"
